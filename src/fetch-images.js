3e123276871e5c97fc7a46ec4e97369b78dcfc19
VictoriaSzn <victoriia.szn@gmail.com>
2023-01-18 16:13:43 +0200

    1_2

--- src/fetch-images.js
+++ src/fetch-images.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async function fetchImages(value, page) {
-  const BASE_URL = 'https://pixabay.com/api';
+  const BASE_URL = 'https://pixabay.com/api/';
   const KEY = '32852753-8f3b804226363e950fb952518';
   const filter = `?key=${KEY}&q=${value}&image_type=photo
   &orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
